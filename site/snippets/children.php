a0033b03e82320effaf1e106b33ef7060b1509a8
Ethan Shutt <ethan@ethanmadeit>
2019-10-26 17:55:19 -0500

    remove cms, basic structure

--- site/snippets/children.php
+++
@@ -1,9 +0,0 @@
-<?php foreach ($site->children()->visible() as $section): ?>
-  <section class="section" id="<?= $section->title()->html() ?>">
-    <div class="container">
-      <h3><?= $section->title() ?></h3>
-      <?= $section->intro()->kirbytext() ?>
-      <a class="button" href="<?= $section->url() ?>">Read On..</a>
-    </div>
-  </section>
-<?php endforeach ?>
